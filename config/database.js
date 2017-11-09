400385c182b198afd25ddbb3c547103c8b654b1c
Sai Krishna Karanam <krishnakaranam3732@gmail.com>
2017-11-09 11:40:57 -0300

    db update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // configuration of database.js
 module.exports = {
 
-    'url' : 'mongodb://heroku_xswszw71:fflvnn2gl1uegr23u5nkv8vft6@ds153015.mlab.com:53015/heroku_xswszw71'
+    'url' : 'mongodb://heroku_xptmg5hr:9sp2bqojd2obgiccnlujse1mo@ds143241.mlab.com:43241/heroku_xptmg5hr'
 
 };
