3b027181c0a2218a819655fbf91cf68ea1de7e42
Sai Krishna Karanam <krishnakaranam3732@gmail.com>
2017-11-09 11:34:10 -0300

    twitter update

--- config/auth.js
+++ config/auth.js
@@ -6,14 +6,14 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '1983707671915939', // Facebook App ID
         'clientSecret'    : 'e385cc141d50844b9fb31703fbf8cb46', // Facebook App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'     : 'https://karanam-saikrishna-webdev.herokuapp.com/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
     },
 
     'twitterAuth' : {
-        'consumerKey'        : 'your-consumer-key-here',
-        'consumerSecret'     : 'your-client-secret-here',
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+        'consumerKey'        : '7WjFZXEH9Rgu31ojkOt8fzqJK',
+        'consumerSecret'     : 'BbekTHRAPHr3AZEEVPPGiy1PJA92ltRhAEFZUykCHVHXppt6DV',
+        'callbackURL'        : 'https://karanam-saikrishna-webdev.herokuapp.com/auth/twitter/callback'
     }
 };
