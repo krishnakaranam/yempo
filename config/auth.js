7919afbbe639279359d12f6efb63411a12b14f12
Sai Krishna Karanam <krishnakaranam3732@gmail.com>
2017-11-09 00:25:29 -0300

    update

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'        : '165425237376783', // your App ID
-        'clientSecret'    : 'c5f4236a5f5abf97abb161915ae702a7', // your App Secret
+        'clientID'        : '1983707671915939', // your App ID
+        'clientSecret'    : 'e385cc141d50844b9fb31703fbf8cb46', // your App Secret
         'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
