2e0177b66ddf3b45bb5c235a4ff610ffda679af7
krishnakaranam <karanam.s@husky.neu.edu>
2018-04-19 17:06:54 -0400

    update

--- config/config.js
+++ config/config.js
@@ -7,6 +7,6 @@
 module.exports = {
     consumer_key:         'VK7j7cv1FS48gtc6ZuL81Al1v', 
     consumer_secret:      'tVDWgyqhd8oRzxRq9VzlM7gr4az5WPHHdSEfn9eZOdVzUWCmv5',
-    access_token:         '	1478790870-zazptd06RDDs7TRz0QcAgWHxJrCZQECYL8QFwFi',
+    access_token:         '1478790870-zazptd06RDDs7TRz0QcAgWHxJrCZQECYL8QFwFi',
     access_token_secret:  'C9TBvxnXwmh7UPFpzPIYXq5WkdZ6q65mxaunrRK6avApN'
   }
