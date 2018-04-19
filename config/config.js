d8aa8a86a0a103552182556cca2197e8967d6aec
krishnakaranam <karanam.s@husky.neu.edu>
2018-04-19 16:47:46 -0400

    consumer key reset

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@
 // var config = require('./config.js');
 
 module.exports = {
-    consumer_key:         'd8xfh5N7dCQKG89kZB10OUSin', 
-    consumer_secret:      'OHKq4wWgQg3pQjoFyHLDBY7EEbO0UCRupgQ6Re7OEQDpuBxje3',
+    consumer_key:         'VK7j7cv1FS48gtc6ZuL81Al1v', 
+    consumer_secret:      'tVDWgyqhd8oRzxRq9VzlM7gr4az5WPHHdSEfn9eZOdVzUWCmv5',
     access_token:         '1478790870-dW36ujNkJiQpHVJatXsDwFFNDHoKPe941CI1PYK',
     access_token_secret:  'skcLxRmDEbGRbiZ3vpRofLpBg745WaqqOCMZsS1wnOvxn'
   }
